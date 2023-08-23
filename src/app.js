b9c1a5b304a777d3dabc0f982212363de69fc698
amangiri7 <amangiri727@gmail.com>
2023-08-23 22:07:50 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -1,28 +1,62 @@
+// Import required modules
+const express = require("express");
+const path = require("path");
+const Subscriber = require("./models/subscribers");
 
-const express = require('express');
-const app = express()
+// Create an instance of the Express application
+const app = express();
 
+// To use static files we need to give permission of the public folder
+app.use(express.static("public"));
 
-// Your code goes here
+// Routes
+// API to render html file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
+// API to get all data of subscribers
+app.get("/subscribers", async (req, res) => {
+  try {
+    let subscribers = await Subscriber.find();
+    //Response data
+    res.status(200).send(subscribers);
+  } catch (error) {
+    res.status(500);
+  }
+});
 
+// API to get all subscribers by name and subscribedChannel
+app.get("/subscribers/names", async (req, res) => {
+  try {
+    let subscribers = await Subscriber.find(
+      {},
+      { name: 1, subscribedChannel: 1, _id: 0 }
+    );
+    //Response data
+    res.status(200).send(subscribers);
+  } catch (error) {
+    res.status(500);
+  }
+});
 
+// API to get subscribers by id
+app.get("/subscribers/:id", async (req, res) => {
+  try {
+    let subscribers = await Subscriber.findById(req.params.id);
+    // Response data
+    res.status(200).send(subscribers);
+  } catch (error) {
+    // Error status and message
+    res
+      .status(400)
+      .send({ message: "No Subscriber found related to this id." });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Handles all the unwanted request
+app.use((req, res) => {
+    res.status(404).json({ message: "Error - Route not found" }); // Send a JSON response with a status of 404 (Not Found)
+});
 
 module.exports = app;
