ccad43d2215414d681d89babe9a967fae60eb735
amangiri7 <amangiri727@gmail.com>
2023-08-26 21:10:50 +0530

    third commit

--- src/createDatabase.js
+++ src/createDatabase.js
@@ -1,18 +1,39 @@
+// Import required modules
 const mongoose = require('mongoose')
 const subscriberModel = require('./models/subscribers')
 const data = require('./data')
 
-// Connect to DATABASE
-const DATABASE_URL = "mongodb://localhost:27017/subscribers";
-mongoose.connect(DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true });
-const db = mongoose.connection
-db.on('error', (err) => console.log(err))
-db.once('open', () => console.log('Database created...'))
+// Configuration of env
+require("dotenv").config();
 
+// Connect to DATABASE
+// Local URI
+// const DATABASE_URL = "mongodb://127.0.0.1:27017/subscribers";
+
+// Cluster URI
+const DATABASE_URL = process.env.DATABASE_URI;
+
+//Connect to MongoDB using Mongoose
+mongoose.connect(DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+
+// If an error occurs during connection, handle and log the error
+db.on("error", (err) => console.log(err));
+
+// If the connection is successful, log a success message
+db.once("open", () => console.log("Database created..."));
+
+// Refresh all connections
 const refreshAll = async () => {
-    await subscriberModel.deleteMany({})
-    // console.log(connection)
-    await subscriberModel.insertMany(data)
-    await mongoose.disconnect();
-}
-refreshAll()
+  await subscriberModel.deleteMany({});
+  // console.log("Deleting Previous Data")
+  await subscriberModel.insertMany(data);
+  // console.log("Inserting Data");
+  await mongoose.disconnect();
+  // console.log("Database Disconnect")
+};
+
+refreshAll();
